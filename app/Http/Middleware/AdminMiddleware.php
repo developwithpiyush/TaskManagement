3c82d795e3d2572226132151bbf93f3be0bf68cf
piyush garg <piyushgarg8536@gmail.com>
2026-09-25 10:44:50 +0530

    admin middlewarre create and register, Task policy createdd

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (! auth()->check()) {
+            return redirect()->route('login');
+        }
+
+        if (! auth()->user()->isAdmin()) {
+            abort(403, 'You are not authorized to access this area.');
+        }
+        return $next($request);
+    }
+}
